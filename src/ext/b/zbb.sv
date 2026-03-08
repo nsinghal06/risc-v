4016eb84cff1edf5767d5ce66ecd7e9a32eaa26f
Mohammad <mohammad.shahhaidari@mail.utoronto.ca>
2026-03-08 13:28:55 -0400

    fixed syntax error

--- src/ext/b/zbb.sv
+++ src/ext/b/zbb.sv
@@ -13,8 +13,8 @@ module zbb(
       3'b010: out = ~(a ^ b); //xnor
       3'b011: out = ($signed(a) < $signed(b) ? a : b); //min
       3'b100: out = ($signed(a) < $signed(b) ? b : a); //max
-      3'b101: out = {24{a[7]}, a[7:0]}; //sext.b (sign extend byte)
-      3'b110: out = {16{a[15]}, a[15:0]}; //sext.h (sign extend halfword)
+      3'b101: out = { {24{a[7]}}, a[7:0]}; //sext.b (sign extend byte)
+      3'b110: out = { {16{a[15]}}, a[15:0]}; //sext.h (sign extend halfword)
       3'b111: out = {16'b0, a[15:0]}; //zext.h (zero extend halfword)
       default: out = 32'd0; //other
     endcase
