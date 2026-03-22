fd0a26b7f1c1a1a06cd77d076ff05a885a45924a
Mohammad <mohammad.shahhaidari@mail.utoronto.ca>
2026-03-22 01:24:00 -0400

    Added zbb encodings to the decoder based on https://docs.riscv.org/reference/isa/unpriv/b-st-ext.html#zbb and updated types.svh and the zbb.sv accordingly

--- src/ext/b/decoder.sv
+++ src/ext/b/decoder.sv
@@ -5,13 +5,17 @@ module ext__b__decoder
   , input [6:0] funct7
   , input [6:0] opcode
   , input [4:0] rd
+  , input [4:0] rs2
   , output ext__b__types::b_alu_control_t b_alu_control
   );
 
   import ext__b__types::*;
 
   localparam bit [6:0] FUNCT7_ZBA = 7'b0010000;
-  localparam bit [6:0] FUNCT7_ZBB = 7'b0100000;
+  localparam bit [6:0] FUNCT7_ZBB__LOGICAL = 7'b0100000;
+  localparam bit [6:0] FUNCT7_ZBB__MINMAX = 7'b0000101;
+  localparam bit [6:0] FUNCT7_ZBB__SEXT = 7'b0110000;
+  localparam bit [6:0] FUNCT7_ZBB__ZEXT = 7'b0000100;
 
   always_comb
     case (opcode)
@@ -25,19 +29,47 @@ module ext__b__decoder
               default: b_alu_control = B_ALU_CTRL__NONE;
             endcase
 
-          FUNCT7_ZBB:
+          FUNCT7_ZBB__LOGICAL:
             case (funct3)
               3'b111:  b_alu_control = B_ALU_CTRL__ANDN;
               3'b110:  b_alu_control = B_ALU_CTRL__ORN;
               3'b100:  b_alu_control = B_ALU_CTRL__XNOR;
               default: b_alu_control = B_ALU_CTRL__NONE;
             endcase
-
+          FUNCT7_ZBB__MINMAX:
+            case (funct3)
+              3'b100:  b_alu_control = B_ALU_CTRL__MIN;
+              3'b110:  b_alu_control = B_ALU_CTRL__MAX;
+              default: b_alu_control = B_ALU_CTRL__NONE;
+            endcase
+          FUNCT7_ZBB__ZEXT:
+            case (funct3)
+              3'b100:  
+                case(rs2)
+                  5'b00000: b_alu_control = B_ALU_CTRL__ZEXTH;
+                  default: b_alu_control = B_ALU_CTRL__NONE;
+                endcase
+              default: b_alu_control = B_ALU_CTRL__NONE;
+            endcase
           // TODO: Implement zbs into ALU decoder, also confirm what zbb instructions are being implemented.
           default: b_alu_control = B_ALU_CTRL__NONE;
 
         endcase
+      7'b0010011:
+        case (funct7)
+          FUNCT7_ZBB__SEXT:
+            case (funct3)
+              3'b001:
+                case (rs2)
+                  5'b00100:  b_alu_control = B_ALU_CTRL__SEXTB;
+                  5'b00101:  b_alu_control = B_ALU_CTRL__SEXTH;
+                  default: b_alu_control = B_ALU_CTRL__NONE;
+                endcase
+              default: b_alu_control = B_ALU_CTRL__NONE;
+            endcase
+          default: b_alu_control = B_ALU_CTRL__NONE;
 
+        endcase
       default: b_alu_control = B_ALU_CTRL__NONE;
     endcase
 
--- src/ext/b/zbb.sv
+++ src/ext/b/zbb.sv
@@ -1,21 +1,23 @@
+`include "src/ext/b/types.svh"
+
 module zbb(
     input [31:0] a
   , input [31:0] b
-  , input [2:0] zbb_control
+  , input ext__b__types::b_alu_control_t b_alu_control
   , output reg [31:0] out
   , output zeroE
   );
 
   always_comb
-    case (zbb_control)
-      3'b000: out = a & ~b; //andn
-      3'b001: out = a | ~b; //orn
-      3'b010: out = ~(a ^ b); //xnor
-      3'b011: out = ($signed(a) < $signed(b) ? a : b); //min
-      3'b100: out = ($signed(a) < $signed(b) ? b : a); //max
-      3'b101: out = { {24{a[7]}}, a[7:0]}; //sext.b (sign extend byte)
-      3'b110: out = { {16{a[15]}}, a[15:0]}; //sext.h (sign extend halfword)
-      3'b111: out = {16'b0, a[15:0]}; //zext.h (zero extend halfword)
+    case (b_alu_control)
+      B_ALU_CTRL__ANDN: out = a & ~b; //andn
+      B_ALU_CTRL__ORN: out = a | ~b; //orn
+      B_ALU_CTRL__XNOR: out = ~(a ^ b); //xnor
+      B_ALU_CTRL__MIN: out = ($signed(a) < $signed(b) ? a : b); //min
+      B_ALU_CTRL__MAX: out = ($signed(a) < $signed(b) ? b : a); //max
+      B_ALU_CTRL__SEXTB: out = { {24{a[7]}}, a[7:0]}; //sext.b (sign extend byte)
+      B_ALU_CTRL__SEXTH: out = { {16{a[15]}}, a[15:0]}; //sext.h (sign extend halfword)
+      B_ALU_CTRL__ZEXTH: out = {16'b0, a[15:0]}; //zext.h (zero extend halfword)
       default: out = 32'd0; //other
     endcase
 
