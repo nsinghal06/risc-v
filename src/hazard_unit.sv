4cb037ca2a814f2fcf3acdc26b82b927071be306
DanielTaoHuang123 <danielthuang@outlook.com>
2026-03-29 15:21:47 -0400

    make hazard unit combinationally

--- src/hazard_unit.sv
+++ src/hazard_unit.sv
@@ -4,60 +4,61 @@
 import pkg_hazard_unit::*;
 
 module hazard_unit
-  ( input wire clk
-  , input wire [4:0] Rs1E
-  , input wire [4:0] Rs2E
-  , input wire [4:0] RdM
-  , input wire [4:0] RdW
-  , input wire RegWriteM
-  , input wire RegWriteW
-  , input write_back_result_src_t ResultSrcE
-  , input wire [4:0] Rs1D
-  , input wire [4:0] Rs2D
-  , input wire [4:0] RdE
-  , input wire PCSrcE
+  ( input  wire clk
+  , input  wire [4:0] Rs1E
+  , input  wire [4:0] Rs2E
+  , input  wire [4:0] RdM
+  , input  wire [4:0] RdW
+  , input  wire RegWriteM
+  , input  wire RegWriteW
+  , input  write_back_result_src_t ResultSrcE
+  , input  wire [4:0] Rs1D
+  , input  wire [4:0] Rs2D
+  , input  wire [4:0] RdE
+  , input  wire PCSrcE
   , output forward_a_t ForwardAE
   , output forward_b_t ForwardBE
-  , output reg lwStall
-  , output reg StallF
-  , output reg StallD
-  , output reg FlushD
-  , output reg FlushE
+  , output logic lwStall
+  , output logic StallF
+  , output logic StallD
+  , output logic FlushD
+  , output logic FlushE
   );
 
   wire ResultSrcE0;
   assign ResultSrcE0 = ResultSrcE[0];
 
-//Forward
-// TODO: check if we need to do this combinationally
-// probably need to do it combinationally to avoid unnecessary stalls
-  always_comb
-    if (((Rs1E == RdM) & RegWriteM) & (Rs1E != 0))
+  // Forwarding
+  always_comb begin
+    if ((Rs1E == RdM) && RegWriteM && (Rs1E != 5'd0))
       ForwardAE = FORWARD_A__MEMORY_ALU_RESULT;
-    else if (((Rs1E == RdW) & RegWriteW) & (Rs1E != 0))
+    else if ((Rs1E == RdW) && RegWriteW && (Rs1E != 5'd0))
       ForwardAE = FORWARD_A__WRITE_BACK_RESULT;
     else
       ForwardAE = FORWARD_A__EXECUTE_RD1;
+  end
 
-  always_comb
-    if (Rs2E != 0 && Rs2E == RdM && RegWriteM)
+  always_comb begin
+    if ((Rs2E == RdM) && RegWriteM && (Rs2E != 5'd0))
       ForwardBE = FORWARD_B__MEMORY_ALU_RESULT;
-    else if (Rs2E != 0 && Rs2E == RdW && RegWriteW)
+    else if ((Rs2E == RdW) && RegWriteW && (Rs2E != 5'd0))
       ForwardBE = FORWARD_B__WRITE_BACK_RESULT;
     else
       ForwardBE = FORWARD_B__EXECUTE_RD2;
-
-//Stall when a load hazard occurs
-  always @ (posedge clk) begin
-    lwStall <= ResultSrcE0 & ((Rs1D == RdE) | (Rs2D == RdE));
-    StallF <= lwStall;
-    StallD <= lwStall;
   end
 
-//Flush when a control hazard occurs
-  always @ (posedge clk) begin
-    FlushD <= PCSrcE;
-    FlushE <= lwStall | PCSrcE;
+  //Stall when a load hazard occurs
+  always_comb begin
+    lwStall = ResultSrcE0 && ((Rs1D == RdE) || (Rs2D == RdE)) && (RdE != 5'd0);
+
+    StallF = lwStall;
+    StallD = lwStall;
+  end
+
+  //Flush when a control hazard occurs
+  always_comb begin
+    FlushD = PCSrcE;
+    FlushE = lwStall || PCSrcE;
   end
 
 endmodule
