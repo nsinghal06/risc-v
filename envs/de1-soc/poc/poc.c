2fd678910d572fdc92bd7e0cb95362ce1b1b185a
Boris <boris.potapov@mail.utoronto.ca>
2026-05-10 04:20:16 +0000

    clean up ws

--- envs/de1-soc/poc/poc.c
+++ envs/de1-soc/poc/poc.c
@@ -4,8 +4,8 @@ int main(void) {
     register int cnt = 0;
 
     while (1) {
-        cnt = (cnt + 1) & 0x3FF;   
-        *LEDR = cnt;               
+        cnt = (cnt + 1) & 0x3FF;
+        *LEDR = cnt;
 
         for (volatile int i = 0; i<1000000; i++){
             //delay
