c8a887bc55c85e181537ab2b2c3a05f07397f83f
SenseiPrimexus <kevin20383sabis@gmail.com>
2026-06-09 18:53:19 -0400

    Add funct3 input to control_fsm and update result_src logic for CSR support

--- src/modules/ControlFSM.sv
+++ src/modules/ControlFSM.sv
@@ -6,6 +6,7 @@
 module control_fsm
 /* verilator lint_off DECLFILENAME */
   ( input opcode_t opcode
+  , input logic [2:0] funct3
 
   , output var logic        reg_write
   , output result_src_t     result_src
@@ -26,7 +27,11 @@ module control_fsm
       (opcode == IType_logic) ||
       (opcode == IType_jalr) ||
       (opcode == UType_auipc) ||
-      (opcode == UType_lui);
+      (opcode == UType_lui)
+`ifdef UTOSS_RISCV__ZICSR_ENABLED
+      || ((opcode == SYSTEM) && (funct3 inside {3'b001, 3'b010, 3'b011}))
+`endif
+      ;
 
   always_comb
     case (opcode)
@@ -36,6 +41,13 @@ module control_fsm
         result_src = RESULT_SRC__READ_DATA;
       JType, IType_jalr:
         result_src = RESULT_SRC__PC_PLUS_4;
+`ifdef UTOSS_RISCV__ZICSR_ENABLED
+      SYSTEM:
+        if (funct3 inside {3'b001, 3'b010, 3'b011})
+          result_src = RESULT_SRC__CSR_READ;
+        else
+          result_src = result_src_t'('0);
+`endif
       default:
         result_src = result_src_t'('0);
     endcase
@@ -59,6 +71,10 @@ module control_fsm
         alu_src_a = ALU_SRC_A__RD1;
       UType_auipc, JType:
         alu_src_a = ALU_SRC_A__PC;
+`ifdef UTOSS_RISCV__ZICSR_ENABLED
+      SYSTEM:
+        alu_src_a = ALU_SRC_A__RD1;
+`endif
       default:
         alu_src_a = alu_src_a_t'('x);
     endcase
@@ -69,6 +85,10 @@ module control_fsm
         alu_src_b = ALU_SRC_B__RD2;
       UType_auipc, UType_lui, IType_logic, IType_jalr, IType_load, SType:
         alu_src_b = ALU_SRC_B__IMM_EXT;
+`ifdef UTOSS_RISCV__ZICSR_ENABLED
+      SYSTEM:
+        alu_src_b = ALU_SRC_B__IMM_EXT;
+`endif
       default:
         alu_src_b = alu_src_b_t'('x);
     endcase
